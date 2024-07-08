32e80162722db9a55f65b0d15630cd0fce759b46
Jakub Werykowski <werykowiak@gmail.com>
2024-07-08 13:27:22 +0200

    wyswietlanie zamowien i pozycji

--- FoodOrderApp/Models/OrderPositionModel.cs
+++ FoodOrderApp/Models/OrderPositionModel.cs
@@ -5,10 +5,11 @@ namespace FoodOrderApp.Models
     public class OrderPositionModel
     {
         [Key]
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string User { get; set; }
         public string Position { get; set; }
-        public string Additives { get; set; }
-        public int Cost { get; set; }
+        public string? Additives { get; set; }
+        public double? Cost { get; set; }
+        public int? OrderId { get; set; }
     }
 }
