2ba1fbfae771fae22d90fa8cdc9c7aae2b9ca90f
Werykowiak <88315629+Werykowiak@users.noreply.github.com>
2024-07-21 18:10:44 +0200

    poprawa pobierania pozycji

--- FoodOrderApp/Program.cs
+++ FoodOrderApp/Program.cs
@@ -5,19 +5,30 @@ using Microsoft.Fast.Components.FluentUI;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Graph;
 using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.NumberHandling = System.Text.Json.Serialization.JsonNumberHandling.AllowNamedFloatingPointLiterals;
+    });
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 var config = builder.Configuration.Get<ConfigOptions>();
 builder.Services.AddTeamsFx(config.TeamsFx.Authentication);
 builder.Services.AddScoped<MicrosoftTeams>();
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddFluentUIComponents();
-
-
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
 builder.Services.AddControllers();
 //builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
 builder.Services.AddHttpClient("FoodOrderApi", client =>
@@ -26,12 +37,6 @@ builder.Services.AddHttpClient("FoodOrderApi", client =>
 });
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddAntiforgery(o => o.SuppressXFrameOptionsHeader = true);
-
-/*var baseUrl = builder.Configuration.GetSection("MicrosoftGraph")["BaseUrl"];
-var scopes = builder.Configuration.GetSection("MicrosoftGraph:Scopes")
-    .Get<List<string>>();
-
-builder.Services.AddGraphClient(baseUrl, scopes);*/
 
 var app = builder.Build();
 
@@ -45,7 +50,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -57,24 +62,3 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-
-public class GraphClientHandler : DelegatingHandler
-{
-    private readonly IAccessTokenProvider _tokenProvider;
-
-    public GraphClientHandler(IAccessTokenProvider tokenProvider)
-    {
-        _tokenProvider = tokenProvider;
-    }
-
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var result = await _tokenProvider.RequestAccessToken();
-        if (result.TryGetToken(out var token))
-        {
-            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.Value);
-        }
-
-        return await base.SendAsync(request, cancellationToken);
-    }
-}
